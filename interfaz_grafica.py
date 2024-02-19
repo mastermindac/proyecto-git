89d45a825e3e196a3f912f40b5c573252c5c98df
DaniFL <danielfernandezlopez75@gmail.com>
2024-02-19 19:26:31 +0100

    Creación GUI en python (interfaz_grafica.py)

---
+++ interfaz_grafica.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+from tkinter import filedialog
+
+def imprimir_contenido_archivo(ruta_archivo):
+    try:
+        with open(ruta_archivo, 'r') as archivo:
+            contenido = archivo.read()
+            return contenido
+    except FileNotFoundError:
+        return f"El archivo '{ruta_archivo}' no fue encontrado."
+    except Exception as e:
+        return f"Ocurrió un error: {e}"
+
+def abrir_archivo():
+    ruta_archivo = filedialog.askopenfilename(title="Seleccionar archivo", filetypes=[("Archivos de texto", "*.txt")])
+    if ruta_archivo:
+        contenido = imprimir_contenido_archivo(ruta_archivo)
+        mostrar_contenido_ventana(contenido)
+
+def mostrar_contenido_ventana(contenido):
+    ventana = tk.Tk()
+    ventana.title("Contenido del archivo")
+
+    etiqueta = tk.Label(ventana, text=contenido, padx=10, pady=10)
+    etiqueta.pack()
+
+    ventana.mainloop()
+
+# Ejemplo de uso
+abrir_archivo()
